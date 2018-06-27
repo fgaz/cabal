7b00f38d21f0ee45893dc80d3861a1a07a177ac8
Francesco Gazzetta <francygazz@gmail.com>
2018-06-27 11:13:31 +0200

    Make --dependency accept a component too
    The old --dependency could only do --dependency=pkg=cid,
    but with public sublibraries this will become insufficient.
    
    Now there is the option to also specify a component name using
    --dependency=pkg:component=cid

--- cabal-install/Distribution/Client/ProjectPlanning.hs
+++ cabal-install/Distribution/Client/ProjectPlanning.hs
@@ -3270,6 +3270,7 @@ setupHsConfigureFlags (ReadyPackage elab@ElaboratedConfiguredPackage{..})
                                         | packageId elab == srcid
                                         -> mkPackageName (unUnqualComponentName uqn)
                                     _ -> packageName srcid,
+                                   CLibName,
                                    cid)
                                 | ConfiguredId srcid mb_cn cid <- elabLibDependencies elab ]
     configConstraints         =
